fe98b86128391fb935fb7af1a711299b60c412f6
Aditya Verma <aaadarths2357@gmail.com>
2020-10-27 22:47:16 +0530

    Added: code to initialize employee payroll service with file io and corresponding test cases

---
+++ src/test/java/com/bridgelabz/payrollservicetest/EmployeePayrollServiceTest.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.payrollservicetest;
+
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Test;
+import com.bridgelabz.model.EmployeePayrollData;
+import com.bridgelabz.payrollservice.EmployeePayrollService;
+import com.bridgelabz.payrollservice.EmployeePayrollService.IOService;
+
+public class EmployeePayrollServiceTest {
+
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchNumberOfEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmployees = {
+				new EmployeePayrollData(1, "Aditya Verma", 800000.0),
+				new EmployeePayrollData(2, "Akhil Singh", 850000.0),
+				new EmployeePayrollData(3, "Anamika Bhatt", 900000.0) };
+
+		EmployeePayrollService payrollServiceObject = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+		payrollServiceObject.writeEmployeeData(IOService.FILE_IO);
+		payrollServiceObject.printEmployeePayrollData();
+
+		Assert.assertEquals(3, payrollServiceObject.countEnteries(IOService.FILE_IO));
+	}
+
+	@Test
+	public void given3EmployeesWhenReadFromFileShouldMatchNumberOfEmployeeEntries() {
+
+		EmployeePayrollService payrollServiceObject = new EmployeePayrollService();
+		payrollServiceObject.readEmployeeData(IOService.FILE_IO);
+		int countOfEntriesRead = payrollServiceObject.sizeOfEmployeeList();
+		Assert.assertEquals(3, countOfEntriesRead);
+	}
+
+}
